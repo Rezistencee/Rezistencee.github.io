51e365e7366d0d00c087b3e0a27b72bff3cfbca0
Rezistencee <86205500+Rezistencee@users.noreply.github.com>
2022-02-04 22:49:34 +0200

    Update test.php

--- php/test.php
+++ php/test.php
@@ -3,7 +3,7 @@ $to = "horbonosa@gmail.com";
 $name = $_POST['name'];
 $from = trim($_POST['email']);
 $message = htmlspecialchars($_POST["msg"]);
-$message = urldecode($message)
+$message = urldecode($message);
 $message = trim($message);
 
 $headers = "From" .$name. "\r\n" ."Reply-To: " .$from;
